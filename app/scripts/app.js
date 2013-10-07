41a76cbe8229face351c6be06996fcb9a5e75be5
fischermatte@gmx.net <fischermatte@gmx.net>
2013-10-08 00:47:25 +0200

    added title info

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,10 +2,14 @@
 
 angular.module('geoludApp', []).
       config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-          $routeProvider.when('/', {templateUrl: 'views/main.html', controller: 'MainCtrl'})
-                .when('/resume', {templateUrl: 'views/resume.html', controller: 'ResumeCtrl'})
-                .when('/skills', {templateUrl: 'views/skills.html', controller: 'SkillCtrl'})
-                .when('/projects', {templateUrl: 'views/projects.html', controller: 'ProjectCtrl'})
-                .when('/contact', {templateUrl: 'views/contact.html', controller: 'ContactCtrl'});
+          $routeProvider.when('/', {templateUrl: 'views/main.html', controller: 'MainCtrl', title: "Home"})
+                .when('/resume', {templateUrl: 'views/resume.html', controller: 'ResumeCtrl', title: "Résumé"})
+                .when('/skills', {templateUrl: 'views/skills.html', controller: 'SkillCtrl', title: "Skills"})
+                .when('/projects', {templateUrl: 'views/projects.html', controller: 'ProjectCtrl', title: "Projects"})
+                .when('/contact', {templateUrl: 'views/contact.html', controller: 'ContactCtrl', title: "Contact"});
           $locationProvider.html5Mode(false);
-        }]);
+        }]).run(['$location', '$rootScope', function($location, $rootScope) {
+    $rootScope.$on('$routeChangeSuccess', function(event, current, previous) {
+      $rootScope.title = current.$$route.title;
+    });
+  }]);
